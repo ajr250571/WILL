a3ad097cc0ec312c12b97da18be13bd5715e7b18
ajr250571 <ajr250571@gmail.com>
2022-10-21 07:56:34 -0300

    prueba

--- prueba.js
+++ prueba.js
@@ -1,64 +1,20 @@
-var personas = [
-  {
-    nombre: 'Emi',
-    edad: 21,
-    hobbies: ['correr', 'dormir', 'nadar'],
-    amigos: [
-      {
-        nombre: 'Martin',
-      }, {
-        nombre: 'Leo',
-      }
-    ],
-  }, {
-    nombre: 'Franco',
-    edad: 22,
-    hobbies: ['futbol', 'golf'],
-    amigos: [
-      {
-        nombre: 'Emi',
-      }, {
-        nombre: 'Jimmy',
-      }, {
-        nombre: 'Matias',
-      }
-    ],
-  },
-  {
-    nombre: 'Martin',
-    edad: 35,
-    hobbies: ['Cancha', 'emprender'],
-    amigos: [
-      {
-        nombre: 'Toni',
-      }, {
-        nombre: 'Leo',
-      }, {
-        nombre: 'Manu',
-      }
-    ],
-  },
-];
+function stringMasLarga(strings) {
+  // La función llamada 'stringMasLarga', recibe como argumento un arreglo de strings llamado 'strings'
+  // y debe devolver el string más largo que hay en el arreglo (Es decir el de mayor cantidad de caracteres)
+  // Ej:
+  // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); debe retornar 'guten tag'
+  // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
-function filtrar(funcion) {
-  let result = []
-  for (let index = 0; index < this.length; index++) {
-    const element = this[index];
-    if (funcion(element)) {
-      result.push(element);
+  // Tu código aca
+  result = ''
+  largo = 0
+  strings.map(x => {
+    if (x.length > largo) {
+      result = x
+      largo = x.length
     }
-  }
-  //this.map(x => {
-  //  if (funcion(x)) {
-  //    result.push(x)
-  //  }
-  //})
+  })
   return result
 }
 
-Array.prototype.filtrar = filtrar
-
-//let result = personas.filter(p => p.edad <= 22).length
-let result = personas.filtrar(p => p.edad <= 22).length
-
-console.log(result)
+console.log(stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']))
